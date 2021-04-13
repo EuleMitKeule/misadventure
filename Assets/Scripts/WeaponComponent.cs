3dc588431db595bb9a42304bd0fbb029e3d32b7b
Ben Korinth <ben.korinth@rub.de>
2021-04-13 21:25:28 +0200

    implemented basic player movement and weapon aiming
    Co-Authored-By: Lennard Beers <47421406+EuleMitKeule@users.noreply.github.com>

---
+++ Assets/Scripts/WeaponComponent.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponComponent : MonoBehaviour
+{
+    /// <summary>
+    /// The radius of the look target's deadzone around the player.
+    /// </summary>
+    [SerializeField] public float deadzoneRadius;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
