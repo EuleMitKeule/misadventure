3738becce7d69816cfc8b69daf380baf5db175a2
mifiamigahna <58632857+mifiamigahna@users.noreply.github.com>
2021-04-13 19:39:20 +0200

    implemented player movement

--- Assets/Scripts/PlayerComponent.cs
+++ Assets/Scripts/PlayerComponent.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,8 +9,11 @@ public class PlayerComponent : MonoBehaviour
 {
     [Header("Physics")]
     [SerializeField] float speed;
+    [SerializeField] float moveDamping;
     [Header("Input")]
     [SerializeField] InputAction walkAction;
+
+    Vector2 InputValue { get; set; }
 
     Rigidbody2D Rigidbody { get; set; }
 
@@ -20,10 +24,22 @@ public class PlayerComponent : MonoBehaviour
 
     void Start()
     {
-        
+        walkAction.Enable();
     }
 
     void Update()
     {
+        ProcessInput();
+    }
+
+    void FixedUpdate()
+    {
+        Rigidbody.velocity = speed * InputValue;
+    }
+
+    void ProcessInput()
+    {
+        var inputValue = walkAction.ReadValue<Vector2>();
+        InputValue = Vector2.MoveTowards(InputValue, inputValue, 1 / moveDamping);
     }
 }
