28111bf2c6d1e163ab6132dab52bce575a0adba1
Gerrit <thegarylp@gmail.com>
2021-04-18 21:28:34 +0200

    first enemy implementation

--- Assets/_Project/HotlineHyrule/Entities/EnemyComponent.cs
+++ Assets/_Project/HotlineHyrule/Entities/EnemyComponent.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using HotlineHyrule.Entities.EnemyStates;
+using UnityEngine;
 
 namespace HotlineHyrule.Entities
 {
@@ -11,5 +14,56 @@ namespace HotlineHyrule.Entities
         /// The enemy's respawn point.
         /// </summary>
         [SerializeField] public Vector2Int respawnPoint;
+        
+        /// <summary>
+        /// The enemy's possible states
+        /// </summary>
+        [System.Serializable]
+        public enum EnemyState
+        {
+            Idle,
+            Watch,
+            Patrol,
+            Defend,
+            Attack,
+            Dying,
+            Dead
+        }
+
+        Dictionary<EnemyState, EnemyStateBaseComponent> stateComponentsMap;
+
+        /// <summary>
+        /// The enemy's current state
+        /// </summary>
+        public EnemyState state = EnemyState.Patrol;
+
+        void Awake()
+        {
+            stateComponentsMap = new Dictionary<EnemyState, EnemyStateBaseComponent>()
+            {
+                {EnemyState.Patrol, GetComponent<EnemyStatePatrolComponent>()}
+            };
+        }
+
+        void FixedUpdate()
+        {
+            stateComponentsMap[state].FixedStateUpdate();
+        }
+
+        void Update()
+        {
+            stateComponentsMap[state].StateUpdate();
+        }
+
+        /// <summary>
+        /// Changes the enemy's state. Also exits the current one and sets up the new one
+        /// </summary>
+        /// <param name="newState">The new state the enemy shall get</param>
+        void ChangeState(EnemyState newState)
+        {
+            stateComponentsMap[state].Exit();
+            state = newState;
+            stateComponentsMap[state].Setup();
+        }
     }
 }
---
+++ Assets/_Project/HotlineHyrule/Entities/EnemyHitComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace HotlineHyrule.Entities
+{
+    public class EnemyHitComponent : MonoBehaviour
+    {
+        void OnCollisionEnter2D(Collision2D other)
+        {
+            if (LayerMask.LayerToName(other.gameObject.layer) != "projectile") return;
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
