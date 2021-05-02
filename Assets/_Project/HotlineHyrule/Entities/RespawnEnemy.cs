ae7991c57a34390910011de832a3607b69874aa8
mitovdh5 <dimitar.mitov@rub.de>
2021-05-02 18:50:31 +0200

    issuie 51
    Enemy prefab random spawn

---
+++ Assets/_Project/HotlineHyrule/Entities/RespawnEnemy.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+using Random = UnityEngine.Random;
+
+namespace HotlineHyrule.Entities
+{
+    public class RespawnEnemy : MonoBehaviour
+    {
+        [SerializeField] Vector3 spawnPos;
+        [SerializeField] GameObject enemyPrefab_rnd;
+        public int intEnemyMaxCount; 
+       
+        // Start is called before the first frame update
+        void Start()
+        {
+            /* Static Spawn
+            spawnPos = new Vector3(7, 3, 0);
+            Debug.Log("SpawnPos: " + spawnPos.x + ", " + spawnPos.y);
+            Instantiate(enemyPrefab_rnd, spawnPos, Quaternion.identity);*/
+            if(intEnemyMaxCount<= 0)
+            {
+                Debug.Log("Error Anazahl Gegner nicht angegeben! Gegneranzahl: " + intEnemyMaxCount);
+            }
+            else
+            {
+                for (int i = 0; i <= intEnemyMaxCount; i++)
+                {
+                    SpawnRandomPrefeb();
+                }
+            }
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void SpawnRandomPrefeb()
+        {
+            int intSpawnPointX = Random.Range(-10, 10);
+            int intSpawnPointY = Random.Range(-15, 15);
+            Vector3 spawnPosition = new Vector3(intSpawnPointX, intSpawnPointY, 0);
+
+            // Instanz von Enemy-prefab an einer randomisierten Position spawnen ohne Rotation.
+            // mit Rotation freischalten spawnPoints[spawnPointIndex].rotation
+            Instantiate(enemyPrefab_rnd, spawnPosition, Quaternion.identity);
+        }
+    }
+}
+
+
