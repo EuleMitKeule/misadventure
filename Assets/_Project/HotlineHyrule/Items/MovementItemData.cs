c261fd76f523bb8b4e28ccd8b7860094926acbd5
Lennard Beers <lennard.beers@rub.de>
2021-05-11 15:49:08 +0200

    added documentation, minor refactors

--- Assets/_Project/HotlineHyrule/Items/MovementItemData.cs
+++ Assets/_Project/HotlineHyrule/Items/MovementItemData.cs
@@ -5,7 +5,13 @@ namespace HotlineHyrule.Items
     [CreateAssetMenu(menuName = "Item/New Movement Item")]
     public class MovementItemData : ConsumableItemData
     {
+        /// <summary>
+        /// Multiplies the absolute movement speed of the entity.
+        /// </summary>
         [SerializeField] public float movementFactor;
+        /// <summary>
+        /// The amount of time the item's effect should last.
+        /// </summary>
         [SerializeField] public int duration;
     }
 }
