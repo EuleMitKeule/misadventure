91ca5ace00c55a66005321397a0d406360100ff0
Lennard Beers <l.beers@outlook.de>
2021-05-30 14:37:06 +0200

    implemented use weapon quest, implemented kill specific quest

---
+++ Assets/_Project/HotlineHyrule/Entities/EntityEventArgs.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+namespace HotlineHyrule.Entities
+{
+    public class EntityEventArgs : EventArgs
+    {
+        public GameObject EntityObject { get; }
+
+        public EntityEventArgs(GameObject entityObject)
+        {
+            EntityObject = entityObject;
+        }
+    }
+}
