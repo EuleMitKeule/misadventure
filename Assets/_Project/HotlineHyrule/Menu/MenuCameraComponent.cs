15f51d39e1b45ae2772b79d74401fe328876cf0c
Lennard Beers <l.beers@outlook.de>
2021-07-28 00:48:49 +0200

    fixed camera start position in menu

--- Assets/_Project/HotlineHyrule/Menu/MenuCameraComponent.cs
+++ Assets/_Project/HotlineHyrule/Menu/MenuCameraComponent.cs
@@ -1,54 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class MenuCameraComponent : MonoBehaviour
+namespace HotlineHyrule.Menu
 {
-    [SerializeField] GameObject waypointObject;
-
-    private int currentIndex;
-    private float speed;
-
-    private Vector3[] waypoints;
-
-    private float z;
-
-    void Awake()
+    public class MenuCameraComponent : MonoBehaviour
     {
-        currentIndex = 0;
-        speed = 1f;
+        [SerializeField] GameObject waypointObject;
 
-        float z = transform.position.z;
+        int currentIndex;
+        float speed;
 
-        transform.position = new Vector3(0, 0, -10);
+        Vector3[] waypoints;
 
-        waypoints = new Vector3[waypointObject.transform.childCount];
-		
-        for (int i = 0; i < waypoints.Length; ++i)
-            waypoints[i] = waypointObject.transform.GetChild(i).position;
+        float z;
 
-    }
+        void Awake()
+        {
+            currentIndex = 0;
+            speed = 1f;
 
-    void Update()
-    {
-        Vector2 target = waypoints[currentIndex];
-        Vector2 position = transform.position;
+            waypoints = new Vector3[waypointObject.transform.childCount];
 
-        Vector2 direction = target - position;
-        direction.Normalize();
+            for (var i = 0; i < waypoints.Length; ++i)
+                waypoints[i] = waypointObject.transform.GetChild(i).position;
 
-        Vector2 moved = direction * speed * Time.deltaTime;
+            transform.position = new Vector3(waypoints[0].x, waypoints[0].y, -10);
+        }
 
-        transform.position += new Vector3(moved.x, moved.y, 0);
+        void Update()
+        {
+            Vector2 target = waypoints[currentIndex];
+            Vector2 position = transform.position;
 
-        Vector2 newPosition = transform.position;
+            Vector2 direction = target - position;
+            direction.Normalize();
 
-        float distance = Vector2.Distance(newPosition, target);
+            Vector2 moved = direction * speed * Time.deltaTime;
 
-        if (distance < 0.01f)
-            if (currentIndex < waypoints.Length - 1)
-                ++currentIndex;
-            else
-                currentIndex = 0;
+            transform.position += new Vector3(moved.x, moved.y, 0);
+
+            Vector2 newPosition = transform.position;
+
+            float distance = Vector2.Distance(newPosition, target);
+
+            if (distance < 0.01f)
+                if (currentIndex < waypoints.Length - 1)
+                    ++currentIndex;
+                else
+                    currentIndex = 0;
+        }
     }
 }
