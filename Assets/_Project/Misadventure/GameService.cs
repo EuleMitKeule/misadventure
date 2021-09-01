f0500543e3ea0454df4e96db94e2d774c935ce59
Lennard Beers <l.beers@outlook.de>
2021-09-01 15:36:46 +0200

    final adjustments

---
+++ Assets/_Project/Misadventure/GameService.cs
@@ -0,0 +1,37 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace HotlineHyrule
+{
+    public class GameService : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject gamePrefab;
+
+        void OnEnable()
+        {
+            DontDestroyOnLoad(gameObject);
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode)
+        {
+            SetupLevel();
+        }
+
+        void SetupLevel()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+            if (Locator.GameComponent) return;
+            // if (!Locator.LevelComponent && SceneManager.GetActiveScene().name != "scene_menu") return;
+
+            // var gamePrefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/_Project/Prefabs/game.prefab");
+            // PrefabUtility.InstantiatePrefab(gamePrefab, SceneManager.GetActiveScene());
+
+            Instantiate(gamePrefab);
+
+            Locator.GameComponent.ReloadScene();
+        }
+    }
+}
